9a8beeea320771c961fd85924d627c7dba8bf5c3
LiamSheard <Liam.sheard95@gmail.com>
2025-10-11 13:31:27 -0700

    Added code for Login in AuthServices, and DB basic infrastructure to support.

--- CEIS400Project/ECS/Application/AuthService.cs
+++ CEIS400Project/ECS/Application/AuthService.cs
@@ -1,6 +1,48 @@
+using ECS.Infrastructure; // for referencing Employee records stored in local memory
+using ECS.Domain; // for Employee classes. 
+
 namespace ECS.Application
 {
     public class AuthService
     {
+        private readonly EmployeeRepository repoRO; // just for checking entries and ID, not for making changes 
+
+        public AuthService (EmployeeRepository employeeRepository)
+        {
+            repoRO = employeeRepository; 
+        }
+
+        // login method 
+        public (bool Valid, Guid? EmployeeId , string Message) Login(string username, string password)
+        {
+            // quick input valid - check for empty field
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+             return (false, null, "Missing username or password"); 
+
+            //clean whitespaces just to be safe...
+            username = username.Trim();
+            password = password.Trim();
+
+            //lookup reference in EmployeeRepo... Should have User and Password, along with ID, and other Emp info. 
+            var empRefPW = repoRO.CheckEmployeePassword(username);
+
+            //Check Username is correct based on lookup - if didn't find, must not be right or doesn't exist. 
+            if (empRefPW is null) 
+                return (false, null, "Invalid username or password");
+            
+            // Check employees password is correct for the emp record retrieved from DB. 
+            bool matches = string.Equals(empRefPW, password, StringComparison.Ordinal);
+            if (!matches)
+                return (false, null, "Invalid Username or Password");
+            //if Username works, and Password matches, we have valid login!
+            return (true, null, "Login Sucessfull");
+
+
+
+
+
+        }
+
+
     }
 }
--- CEIS400Project/ECS/Infrastructure/EmployeeRepository.cs
+++ CEIS400Project/ECS/Infrastructure/EmployeeRepository.cs
@@ -1,6 +1,35 @@
+
+using ECS.Domain; 
+
 namespace ECS.Infrastructure
 {
     public class EmployeeRepository
     {
+        private class EmployeeRecord
+        {
+            public Employee Employee { get; set; } = default!;
+            public string Username { get; set; } = default!;
+            public string Password { get; set; } = default!;
+
+            public EmployeeRecord(Employee emp, string user, string pass)
+            {
+                Employee = emp;
+                Username = user;
+                Password = pass;
+
+            }
+        }
+        // The List of internal example records
+        private readonly List<EmployeeRecord> testDB = [];
+
+        //Method for matching Employee Username, search DB, and return Emp record if found. 
+        internal string? CheckEmployeePassword(string username)
+        {
+            var record = testDB.FirstOrDefault(x => x.Username == username);
+            var pass = record?.Password;
+            return pass;
+        }
+
+
     }
 }
